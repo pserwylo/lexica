a4f91973a17b27be368206fba22078cb2816682c
Peter Serwylo <peter@serwylo.com>
2021-10-13 13:40:47 +1100

    WIP

--- assets/dictionaries/dictionary.de_DE_no_diacritics.build.sh
+++ assets/dictionaries/dictionary.de_DE_no_diacritics.build.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
+#Needed to have a deterministic outcome regarding the sort order. Otherwise dependant on the computer settings. 
+export LC_ALL=en_US.utf8
+
 DIR=$(dirname "$0")
-
-DICT_FILE_DIACRITICS="$DIR/dictionary.de_DE.txt"
-LICENSE_FILE_DIACRITICS="$DIR/dictionary.de_DE.LICENSE"
-
+BUILD_DIR="$DIR/build"
 DICT_FILE="$DIR/dictionary.de_DE_no_diacritics.txt"
 LICENSE_FILE="$DIR/dictionary.de_DE_no_diacritics.LICENSE"
 
-if [ ! -f "$DICT_FILE_DIACRITICS" ]
+if [ -d "$BUILD_DIR" ]
 then
-    echo "Source dictionary $DICT_FILE_DIACRITICS does not exist. Perhaps you need to run dictionary.de_DE.build.sh first?"
+    echo "Build directory $BUILD_DIR is not empty. Please remove first."
     exit 1
 fi
 
-echo "Copying source dictionary containing diacritics from $DICT_FILE_DIACRITICS..."
-cp "$DICT_FILE_DIACRITICS" "$DICT_FILE"
-cp "$LICENSE_FILE_DIACRITICS" "$LICENSE_FILE"
+echo "Fetching German wordlist..."
+git clone --depth=1 --quiet https://github.com/enz/german-wordlist "$BUILD_DIR"
 
-echo "Replacing ä/Ä->ae, ö/Ö->oe, ü/Ü->ue..."
-sed -i "s/ä/ae/g" "$DICT_FILE"
-sed -i "s/Ä/ae/g" "$DICT_FILE"
-sed -i "s/ö/oe/g" "$DICT_FILE"
-sed -i "s/Ö/oe/g" "$DICT_FILE"
-sed -i "s/ü/ue/g" "$DICT_FILE"
-sed -i "s/Ü/ue/g" "$DICT_FILE"
+echo "Correcting words (i.a. lowering), expanding diacritics (ä/Ä->ae, ö/Ö->oe, ü/Ü->ue, ...), trimming to words between 3 to 9 letters long (inclusive), then removing duplicates introduced by expanding diacritics, and dumping dictionary to $DICT_FILE..."
+cat "$BUILD_DIR/words" | tr '[:upper:]' '[:lower:]' | sort | uniq \
+  | grep -v -P "[àáâåÅçčéèêēëīíïîłñōóõœŒšŠūûú]" \
+  | sed "s/ß/ss/g" \
+  | sed "s/ä/ae/g" \
+  | sed "s/Ä/ae/g" \
+  | sed "s/ö/oe/g" \
+  | sed "s/Ö/oe/g" \
+  | sed "s/ü/ue/g" \
+  | sed "s/Ü/ue/g" \
+  | awk 'length($0) < 10 && length($0) > 2' \
+  | sort | uniq \
+  > "$DICT_FILE"
 
-echo "Removing duplicate words introduced after transforming characters with diacritics."
-cat "$DICT_FILE" | sort | uniq > "$DICT_FILE.tmp" && mv "$DICT_FILE.tmp" "$DICT_FILE" || rm -f "$DICT_FILE.tmp"
+echo "Copying license from source repo..."
+cp "$BUILD_DIR/COPYING" "$LICENSE_FILE"
+
+echo "Removing build directory '$BUILD_DIR'..."
+rm -rf "$BUILD_DIR"
